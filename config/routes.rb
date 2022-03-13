375fcc5ec94bbfb4115944700adf18f55a9d4e4a
Ebina-S <8s6s7e@gmail.com>
2022-03-13 07:58:41 +0000

    初期値エラーあり

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,15 @@ Rails.application.routes.draw do
   
   get 'search' => 'books#search'
   
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: "home#index"
