c9b8d361a80737556a43a6c69e92d71a1639dc91
kizunas <8s6s7e@gmail.com>
2022-05-26 15:27:16 +0900

    APIの検索条件

--- app/controllers/rakutenbooks_controller.rb
+++ app/controllers/rakutenbooks_controller.rb
@@ -8,9 +8,10 @@ PER = 30
     
     def index
         if params[:keyword].present?
-          books = RakutenWebService::Books::Book.search(title: params[:keyword], sort: '-releaseDate')
-          bookpagecount = books.response["pageCount"]
+          @rakutenbooks = RakutenWebService::Books::Book.search(title: params[:keyword], sort: '-releaseDate')
+          bookpagecount = @rakutenbooks.response["pageCount"]
           @books_full = []
+          #ページネーション分岐
           if bookpagecount >= 2
             (bookpagecount).times do |i|
               @booksup = RakutenWebService::Books::Book.search(title: params[:keyword], sort: '-releaseDate', page: i+1)
@@ -20,11 +21,19 @@ PER = 30
             end
             @books = Kaminari.paginate_array(@books_full).page(params[:page]).per(PER)
           else
-            @books = books.page(params[:page]).per(PER)
+            @rakutenbooks.each do |book|
+              @books_full.push(book)
+            end
+            #例外処理
+            begin
+              @books = Kaminari.paginate_array(@books_full).page(params[:page]).per(PER)
+            rescue StandardError => e
+              redirect_to new_rakutenbook_url, alert: e.message
+            end
           end
         else
           respond_to do |format|
-           format.html { redirect_to new_rakutenbook_url, notice: "入力してください" }
+           format.html { redirect_to new_rakutenbook_url, notice: "タイトルを入力してください" }
           end
         end
     end
