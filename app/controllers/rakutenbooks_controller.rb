68d2046e4de735a6eda9ebcc81e04dfe806bc97f
kizunas <8s6s7e@gmail.com>
2022-05-27 13:53:20 +0900

    rakutencontroller修正

--- app/controllers/rakutenbooks_controller.rb
+++ app/controllers/rakutenbooks_controller.rb
@@ -1,41 +1,39 @@
 class RakutenbooksController < ApplicationController
 
-# 表示件数
-PER = 30
+ # 表示件数
+  PER = 30
 
     def new
     end
     
     def index
         if params[:keyword].present?
-          @rakutenbooks = RakutenWebService::Books::Book.search(title: params[:keyword], sort: '-releaseDate')
-          bookpagecount = @rakutenbooks.response["pageCount"]
+          #例外処理(begin省略)
+          @rakuten_books = RakutenWebService::Books::Book.search(title: params[:keyword], sort: '-releaseDate')
+          book_page_count = @rakuten_books.response["pageCount"]
           @books_full = []
           #ページネーション分岐
-          if bookpagecount >= 2
-            (bookpagecount).times do |i|
-              @booksup = RakutenWebService::Books::Book.search(title: params[:keyword], sort: '-releaseDate', page: i+1)
-              @booksup.each do |book|
+          if book_page_count >= 2
+            (book_page_count).times do |i|
+              @books_up = RakutenWebService::Books::Book.search(title: params[:keyword], sort: '-releaseDate', page: i+1)
+              @books_up.each do |book|
                 @books_full.push(book)
               end
             end
             @books = Kaminari.paginate_array(@books_full).page(params[:page]).per(PER)
           else
-            @rakutenbooks.each do |book|
+            @rakuten_books.each do |book|
               @books_full.push(book)
             end
-            #例外処理
-            begin
               @books = Kaminari.paginate_array(@books_full).page(params[:page]).per(PER)
-            rescue StandardError => e
-              redirect_to new_rakutenbook_url, alert: e.message
-            end
           end
         else
           respond_to do |format|
            format.html { redirect_to new_rakutenbook_url, notice: "タイトルを入力してください" }
           end
         end
+      rescue StandardError => e
+        redirect_to new_rakutenbook_url, alert: e.message
     end
 end
 
